4e09ed7343660e3be578167da331ccbcbde19a94
channel960608 <997476274@qq.com>
2022-04-26 19:08:25 -0400

    ADD: docker support

--- docker/build.sh
+++
@@ -1,3 +0,0 @@
-docker build ./DockerfileBaseImage --tag csye7200-base:1.0
-
-docker build --pull --no-cache --tag csye7200-crawler:1.3 - < docker/DockerfileCrawler
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker build . -f docker/DockerfileCrawler -t csye7200-crawler:1.3
+ docker run -it -v ${CONFIG_FILE_PATH}:/app/src/main/resources/application.conf cc311afa397f
